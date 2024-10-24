8d9917a957c0b3ec6c5cd884dd17c42c5ed74afe
jagdish singh boura <bourajagdish1221@gmail.com>
2024-10-24 17:31:34 +0530

    updaed

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript:{
-    ignoreBuildErrors:true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
   experimental: {
-    
-    
-    esmExternals: "loose", 
+    esmExternals: "loose",
     serverComponentsExternalPackages: ["mongoose"],
   },
   eslint: {
@@ -16,24 +14,12 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.clerk.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.clerk.dev",
-      },
-      {
-        protocol: "https",
-        hostname: "uploadthing.com",
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
+      { protocol: "https", hostname: "img.clerk.com" },
+      { protocol: "https", hostname: "images.clerk.dev" },
+      { protocol: "https", hostname: "uploadthing.com" },
+      { protocol: "https", hostname: "placehold.co" },
     ],
   },
 };
 
-export  default nextConfig;
+export default nextConfig;
